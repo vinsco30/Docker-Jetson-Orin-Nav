54cd213fbec7490dd9d9bc618453565d4f40b7a7
Vincenzo Scognamiglio <vincenzo.scognamiglio2@unina.it>
2025-03-25 10:57:20 +0100

    first commit for jetson build

---
+++ run_cnt.sh
@@ -0,0 +1,15 @@
+
+# docker exec -itu 0 px4-ros2 bash  // run root
+
+# enable access to xhost from the container
+xhost +
+
+
+# Run docker and open bash shell 
+docker run --rm -it --privileged \
+-v /tmp/.X11-unix:/tmp/.X11-unix:ro \
+-v "/dev:/dev" \
+-v $(pwd)/ros2_ws-src/pkg:/root/ros2_ws/src:rw \
+--env="DISPLAY=$DISPLAY" \
+--network host \
+--name=jetson-ros2 jetson-ros2 bash
